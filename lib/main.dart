72537a76b3426ab3434ca34547658695a4859700
ForeverYoungs3 <130278688+ForeverYoungs3@users.noreply.github.com>
2023-07-03 22:03:02 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello, World!"),
+      ),
+    ),
+  ));
+}
